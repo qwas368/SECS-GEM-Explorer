2a795145eba397be186b0096a807bd726d323dac
DESKTOP-S2CEEGC\rugini <qwas368@gmail.com>
2019-06-04 21:08:20 +0800

    add revealIine and revealPosition function

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,49 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.registerTreeDataProvider('secs-messages', new SecsMessageProvider(activeTextEditor.document));
         }
     }
+
+    // register
+    registerSecsActive(context);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function registerSecsActive(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.secs.active', () => {
+        revealLine(3);
+      });
+    
+      context.subscriptions.push(disposable);
+}
+
+function revealLine(line: number) {
+    let reviewType: vscode.TextEditorRevealType = vscode.TextEditorRevealType.InCenter;
+    if (vscode.window.activeTextEditor) {
+        if (line === vscode.window.activeTextEditor.selection.active.line) {
+            reviewType = vscode.TextEditorRevealType.InCenterIfOutsideViewport;
+        }
+        const newSe = new vscode.Selection(line, 0, line, 0);
+        vscode.window.activeTextEditor.selection = newSe;
+        vscode.window.activeTextEditor.revealRange(newSe, reviewType);
+    } else {
+        vscode.window.showWarningMessage("Reveal fail. Can't find any active text.");
+    }
+}
+
+function revealPosition(line: number, column: number) {
+
+    if (isNaN(column)) {
+        revealLine(line);
+    } else if(!vscode.window.activeTextEditor) { 
+        vscode.window.showWarningMessage("Reveal fail. Can't find any active text.");
+    } else {
+        let reviewType: vscode.TextEditorRevealType = vscode.TextEditorRevealType.InCenter;
+        if (line === vscode.window.activeTextEditor.selection.active.line) {
+            reviewType = vscode.TextEditorRevealType.InCenterIfOutsideViewport;
+        }
+        const newSe = new vscode.Selection(line, column, line, column);
+        vscode.window.activeTextEditor.selection = newSe;
+        vscode.window.activeTextEditor.revealRange(newSe, reviewType);
+    }
+}
